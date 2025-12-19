91214090e0ca05a832811370141f76a5e2b9b862
Adil Nmili <adile.namili98@gmail.com>
2025-12-19 17:18:38 +0100

    add the requirement boolean on login form

--- src/components/partials/authComponents/loginForm.tsx
+++ src/components/partials/authComponents/loginForm.tsx
@@ -27,6 +27,9 @@ const LoginForm = () => {
         formState: { errors },
     } = useForm<LoginFormData>({
         resolver: zodResolver(loginSchema) as any,
+        defaultValues:{
+            is_backoffice: true
+        }
     })
 
     const onSubmit = async (data: LoginFormData) => {
@@ -65,6 +68,7 @@ const LoginForm = () => {
                         )}
                         
                         <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+                            <input type="hidden" {...register('is_backoffice')} value="true" />
                             {/* Email Field */}
                             <div className="space-y-2">
                                 <Label
